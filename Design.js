766beef003228baf086aeea07c7cfe92434b4714
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2024-10-26 23:13:19 +0530

    some more changes

--- Design.js
+++ Design.js
@@ -16,6 +16,7 @@ btn1.addEventListener("click",()=>{
     if(mode=="on"){
         bg.style.backgroundImage="url('https://wallpaperaccess.com/full/944291.jpg')";
         bg.style.backgroundRepeat="no-repeat";
+        bg.style.backgroundSize="cover";
         mode="off";
         mode1="on";
         mode2="on";
@@ -33,6 +34,7 @@ btn2.addEventListener("click",()=>{
     if(mode1=="on"){
         bg.style.backgroundImage="url('https://th.bing.com/th/id/OIP.XlzitQh6UDoLKpks9O2rlgHaEo?rs=1&pid=ImgDetMain')";
         bg.style.backgroundRepeat="no-repeat";
+        bg.style.backgroundSize="cover";
         mode1="off";
         mode="on";
         mode2="on";
@@ -50,6 +52,7 @@ btn3.addEventListener("click",()=>{
     if(mode2=="on"){
         bg.style.backgroundImage=" url('https://images3.alphacoders.com/100/1007428.jpg')"; 
         bg.style.backgroundRepeat="no-repeat";
+        bg.style.backgroundSize="cover";
         mode2="off";
         mode="on";
         mode1="on";
@@ -67,6 +70,7 @@ btn4.addEventListener("click",()=>{
     if(mode3=="on"){
         bg.style.backgroundImage=" url('https://wallpapercave.com/wp/wp3113575.jpg')"; 
         bg.style.backgroundRepeat="no-repeat";
+        bg.style.backgroundSize="cover";
         mode3="off";
         mode="on";
         mode2="on";
@@ -84,6 +88,7 @@ btn5.addEventListener("click",()=>{
     if(mode4=="on"){
         bg.style.backgroundImage=" url('https://wallpaperaccess.com/full/59110.png')"; 
         bg.style.backgroundRepeat="no-repeat";
+        bg.style.backgroundSize="cover";
         mode4="off";
         mode="on";
         mode2="on";
@@ -101,6 +106,7 @@ btn6.addEventListener("click",()=>{
     if(mode5=="on"){
         bg.style.backgroundImage=" url('https://staticg.sportskeeda.com/editor/2023/05/59948-16831302812524-1920.jpg')"; 
         bg.style.backgroundRepeat="no-repeat";
+        bg.style.backgroundSize="cover";
         mode5="off";
         mode="on";
         mode2="on";
